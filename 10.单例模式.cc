d60ae53508dd205d60a36c08fffc2a7e56af5cd6
xiaoyeaichif <1125035561@qq.com>
2024-11-23 11:31:08 +0800

    锁+双重判断实现的懒汉式单例模式

--- 10.单例模式.cc
+++ 10.单例模式.cc
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <mutex>
-
+#include <memory>
 
 /*
 ** 单例模式的：懒汉式单例模式，饿汉式单例模式
@@ -31,6 +31,7 @@ private:
     static SingLeton1 instance;
 };
 // 类外初始化
+// 在SingLeton1作用域下生成一个instance对象
 SingLeton1 SingLeton1::instance;
 
 
@@ -56,12 +57,53 @@ private:
     // 静态成员函数，返回私有对象
 };
 
+// 懒汉式的第二种写法
+// 锁 + 双重判断的形式
+std::mutex mtx;
 
+class SingLeton3{
+public:
+    // 获取单例对象
+    // 锁 + 双重判断的逻辑
+    static SingLeton3* getInstance(){
+        if(pInstance == nullptr){
+            std::lock_guard<std::mutex>lock(mtx);
+            if(pInstance == nullptr){
+                // 并不知道在何处释放内存
+                // 所以使用智能指针的形式来开辟这片内存
+                //pInstance = new SingLeton3();
+                pInstance = std::make_unique<SingLeton3>();
+            }
+        }
+        // 获取实际的对象SingLeton3
+        return pInstance.get();
+    }
+public:
+    // 实际instance类型为unique_ptr<SingLeton3>，需要显示获取SingLeton3
+    static std::unique_ptr<SingLeton3> pInstance;
+    // 禁止拷贝构造函数和赋值函数
+    SingLeton3& operator=(const SingLeton3&) = delete;
+    SingLeton3(const SingLeton3&) = delete;
+    // 私有构造函数
+    SingLeton3(){
+        std::cout << "懒汉式2---构造函数的调用" << std::endl;
+    }
+    ~SingLeton3(){
+        std::cout << "懒汉式2---析构函数的调用" << std::endl;
+    }
+};
+// 类外初始化
+// 生成智能指针管理的对象
+// 实际instance类型为unique_ptr<SingLeton3>，需要显示获取SingLeton3
+std::unique_ptr<SingLeton3> SingLeton3::pInstance;
 
 int main(){
     // 函数的调用需要注意，只有一个对象被正确的调用
     SingLeton1 *p1 = SingLeton1::getInstance();
     // 引用的方式
     SingLeton2 &p2 = SingLeton2::getInstance();
+    // 指针的形式
+    SingLeton3 *p3 = SingLeton3::getInstance();
+    // delete p3;
     return 0;
 }
